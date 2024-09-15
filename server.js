ac986539565390ea52fcbd08e3590458ac770134
Nikshith reddy <nikshithaaella123@gmail.com>
2024-09-15 18:06:13 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,32 +4,16 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-   myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-
-]
-};
+const budgetData = require("./bdata-source.json")
 
 app.get('/hello', (req, res)=> {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res)=> {
-    res.json(budget);
+    res.json(budgetData);
 });
 
 app.listen(port, () =>{
-    console.log(`"Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
